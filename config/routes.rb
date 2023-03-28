9d791516a7fe1e76f862d93fd146cfa471b1923f
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-28 10:56:00 +0630

    Install the cancancan for the authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'category_details/index'
   get 'categories/index'
-  get 'users/index'
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
