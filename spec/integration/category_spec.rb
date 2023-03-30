6d7c6cd410660a7ac5227f242a3765e3d58ad05f
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-30 11:21:14 +0630

    do the intergration test

---
+++ spec/integration/category_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Food Index', type: :system do
+  before(:each) do
+    @user = User.create(name: 'anye3', email: 'wolo22001@gmail.com', password: '2e2010510',
+                        password_confirmation: '2e2010510')
+    @category = Category.create(user_id: @user.id, name: 'pizza',icon='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Qz_PXUP8Lsb3k6b6tTASlpGQYYCURKlmBA&usqp=CAU')
+  end
+
+  it 'Sign up with a user' do
+    visit new_user_registration_path
+
+    sleep(1)
+    fill_in 'Name', with: @user.name
+    fill_in 'Email', with: @user.email
+    sleep(2)
+    fill_in 'Password', with: @user.password
+    fill_in 'Password confirmation', with: @user.password_confirmation
+    click_button 'Sign up'
+  end
+
+  it 'User should log in' do
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    sleep(2)
+    fill_in 'Password', with: @user.password
+    click_button 'Log in'
+    sleep(5)
+  end
+
+end
